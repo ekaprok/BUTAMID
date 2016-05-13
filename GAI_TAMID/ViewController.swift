9cf393a426175937c24e0214d662d47b69310c38
Екатерина Прокопьева <ekaterinaprokopeva@wireless1x-155-41-97-18.bu.edu>
2016-05-13 15:04:11 -0400

    Initial Commit

---
+++ GAI_TAMID/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GAI_TAMID
+//
+//  Created by Екатерина Прокопьева on 5/13/16.
+//  Copyright © 2016 Екатерина Прокопьева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
