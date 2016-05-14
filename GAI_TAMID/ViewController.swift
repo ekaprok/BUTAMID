9edf26d079472970fd243648d12bb8bdad7a2ce4
Екатерина Прокопьева <ekaterinaprokopeva@Ekaterinas-MacBook-Pro.local>
2016-05-13 20:21:31 -0400

    added files for login without firebase

--- GAI_TAMID/ViewController.swift
+++ GAI_TAMID/ViewController.swift
@@ -8,11 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    let loginButton: FBSDKLoginButton = {
+        let button = FBSDKLoginButton()
+        button.readPermissions = ["email"]
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(loginButton)
+        loginButton.center = view.center
+        loginButton.delegate = self
+        
+        //fetchProfile()
+        //check if logged in before
+        if let token = FBSDKAccessToken.currentAccessToken() {
+            fetchProfile()
+        }
+    }
+    
+    func fetchProfile() {
+        print("Fetch Profile")
+        
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).startWithCompletionHandler { (connection, result, error) -> Void in
+            
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            if let email = result["email"] as? String {
+                print(email)
+            }
+            
+            // unwraps our picture object
+            if let picture = result["picture"] as? NSDictionary, data = picture["data"] as? NSDictionary, url = data["url"] as? String {
+                print(url)
+            }
+        }
+    }
+    
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        print("Completed login")
+        fetchProfile()
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+    }
+    
+    func loginButtonWillLogin(loginButton: FBSDKLoginButton!) -> Bool {
+        return true
     }
 
     override func didReceiveMemoryWarning() {
---
+++ GAI_TAMID/test.m
@@ -0,0 +1,9 @@
+//
+//  test.m
+//  GAI_TAMID
+//
+//  Created by Екатерина Прокопьева on 5/13/16.
+//  Copyright © 2016 Екатерина Прокопьева. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
